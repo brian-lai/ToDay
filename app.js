06e8fb5de9de3e5bc65428f5fe03530ca8868660
unknown <mjwolff@umass.edu>
2015-04-11 16:15:47 -0500

    mongo and routes

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var express = require('express');
 var mongo = require('mongodb');
 var cfenv = require('cfenv');
 
+var users = require('./routes/users');
+var routes = require('./routes/index');
+
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 
@@ -18,6 +21,14 @@ var app = express();
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();

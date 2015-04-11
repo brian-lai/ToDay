ada6823d71c9907546db34e40ad9466ba46d91cd
Michael Markman <mmarkman@umass.edu>
2015-04-11 16:28:37 -0400

    fixed mongo

--- app.js
+++ app.js
@@ -7,11 +7,10 @@
 // This application uses express as it's web server
 // for more info, see: http://expressjs.com
 var express = require('express');
-var mongo = require('mongodb');
-var cfenv = require('cfenv');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
@@ -25,10 +24,6 @@ var appEnv = cfenv.getAppEnv();
 // start server on the specified port and binding host
 app.listen(appEnv.port, appEnv.bind, function() {
 
-	mongodb-toda
-
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
-
-
 });
